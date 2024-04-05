1b278a35fc32be0f2bc352badef7e245f44cb5cc
Meow Roar <sareacct91@gmail.com>
2024-04-05 07:15:54 -0700

    Changed the initializer function for savedId state to be useEffect

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -17,14 +17,15 @@ const SearchBooks = () => {
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
   const [searchInput, setSearchInput] = useState('');
-
   // create state to hold saved bookId values
-  const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+  const [savedBookIds, setSavedBookIds] = useState([]);
 
-  // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
-  // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
-    return () => saveBookIds(savedBookIds);
+    setSavedBookIds(getSavedBookIds);
+  }, []);
+
+  useEffect(() => {
+    saveBookIds(savedBookIds);
   });
 
   // create method to search for books and set state on form submit
