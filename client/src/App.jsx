3c0d4dc28d1cde679988e188b8b5fb8e780cb462
Meow Roar <sareacct91@gmail.com>
2024-04-08 03:59:01 -0700

    refactored App to apollo client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,28 @@
 import './App.css';
-import { Outlet } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
 
+import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
+
+const token = localStorage.getItem('id_token');
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+  headers: {
+    authorization: token ? `Bearer ${token}` : '',
+  },
+})
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
