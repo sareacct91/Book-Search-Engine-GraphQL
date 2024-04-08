a5206023067bdd9462a4d7682b299d3ae924b900
Meow Roar <sareacct91@gmail.com>
2024-04-08 05:29:51 -0700

    added missing description for Book type

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -11,6 +11,7 @@ const typeDefs = `
   type Book {
     _id: ID
     authors: [String]
+    description: String!
     bookId: String!
     image: String
     link: String
@@ -29,7 +30,7 @@ const typeDefs = `
   type Mutation {
     createUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-    saveBook(bookId: String!, title: String!, authors: [String], image: String, link: String): User
+    saveBook(bookId: String!, title: String!, description: String!, authors: [String], image: String, link: String): User
     deleteBook(bookId: String!): User
   }
 `;
